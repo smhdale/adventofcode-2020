678f16f2a4dfaddaaefc2663951d9073f34b11e5
Sam Dale <smhdale@live.com.au>
2020-12-07 21:58:35 +1000

    Set up ts-node and linting

---
+++ 01/index.ts
@@ -0,0 +1,7 @@
+type Test = {
+	myProp: string
+}
+
+module.exports = function () {
+	console.log('hello world!')
+}
---
+++ bin/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env ts-node-script
+
+import fs = require('fs')
+import path = require('path')
+
+const day = (process.argv[2] || '').padStart(2, '0')
+const dir = path.resolve(__dirname, '..', day)
+const exe = path.resolve(dir, 'index.ts')
+
+if (!fs.existsSync(dir) || !fs.existsSync(exe)) {
+	console.log(`\nCouldn't find code for DAY ${day}, aborting.\n`)
+	process.exit(0)
+}
+
+// Load and execute the day's code
+const main = async () => {
+	console.log(`\nRunning code for DAY ${day}...\n`)
+	const script = await import(exe)
+	await script()
+	console.log(``)
+}
+main()
