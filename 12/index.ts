e5b0938c700d8a07a8e7daf267284569d4eddc18
Sam Dale <smhdale@live.com.au>
2020-12-12 22:16:57 +1000

    Rewrite day 12 part A in functional style

--- 12/index.ts
+++ 12/index.ts
@@ -1,82 +1,60 @@
 import { inputAsStringArray, toCartesian, logTest, logAnswer } from '../helpers'
 
 // Setup
-class Ship {
-	x: number
-	y: number
-	bearing: number
-
-	constructor() {
-		this.reset()
-	}
-
-	get manhattanDistance(): number {
-		return Math.abs(this.x) + Math.abs(this.y)
-	}
-
-	private reset() {
-		this.x = 0
-		this.y = 0
-		this.bearing = Math.PI / 2
-	}
-
-	private turn(degrees: number) {
-		this.bearing += (degrees * Math.PI) / 180
-	}
-
-	private move(distance: number) {
-		// Move in direction of current bearing
-		const { x, y } = toCartesian(distance, this.bearing)
-		this.x += Math.round(x)
-		this.y += Math.round(y)
-	}
-
-	private followInstruction(instruction: string) {
-		// Parse instruction
-		const [, dir, rawValue] = instruction.match(/([A-Z])(\d+)/)
-		const value = Number(rawValue)
-
-		switch (dir) {
-			case 'N':
-				this.y -= value
-				break
-			case 'S':
-				this.y += value
-				break
-			case 'E':
-				this.x -= value
-				break
-			case 'W':
-				this.x += value
-				break
-			case 'L':
-				this.turn(-value)
-				break
-			case 'R':
-				this.turn(value)
-				break
-			case 'F':
-				this.move(value)
-				break
-			default:
-				console.log('Unrecognised instruction:', instruction)
-		}
-	}
-
-	navigate(instructions: string[]) {
-		this.reset()
-		instructions.forEach(this.followInstruction.bind(this))
-	}
-}
-
 const TEST_INSTRUCTIONS = inputAsStringArray(__dirname, 'test.txt')
 const REAL_INSTRUCTIONS = inputAsStringArray(__dirname, 'input.txt')
 
-const ferry = new Ship()
+function parseInstruction(raw: string): [string, number] {
+	const [, action, value] = raw.match(/([A-Z])(\d+)/)
+	return [action, Number(value)]
+}
+
+function getManhattanDistance(x: number, y: number): number {
+	return Math.abs(x) + Math.abs(y)
+}
 
 // Part A
-ferry.navigate(TEST_INSTRUCTIONS)
-logTest('A', ferry.manhattanDistance)
+function navigateShip(instructions: string[]): number {
+	// Track ship
+	let x = 0
+	let y = 0
+	let bearing = Math.PI / 2
 
-ferry.navigate(REAL_INSTRUCTIONS)
-logAnswer('A', ferry.manhattanDistance)
+	const move = (distance: number) => {
+		const delta = toCartesian(distance, bearing)
+		x += Math.round(delta.x)
+		y += Math.round(delta.y)
+	}
+
+	for (const instruction of instructions) {
+		const [action, value] = parseInstruction(instruction)
+		switch (action) {
+			case 'N':
+				y -= value
+				break
+			case 'S':
+				y += value
+				break
+			case 'E':
+				x -= value
+				break
+			case 'W':
+				x += value
+				break
+			case 'L':
+				bearing -= (value * Math.PI) / 180
+				break
+			case 'R':
+				bearing += (value * Math.PI) / 180
+				break
+			case 'F':
+				move(value)
+				break
+		}
+	}
+
+	return getManhattanDistance(x, y)
+}
+
+logTest('A', navigateShip(TEST_INSTRUCTIONS))
+logAnswer('A', navigateShip(REAL_INSTRUCTIONS))
